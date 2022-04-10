8336c4124ebf1e0a2ecfc1a835608d2eb3f52b47
hyoungwonkang <hwplus@gmail.com>
2022-04-10 22:17:27 +0900

    Grid, Text 태그 일부 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,7 +8,7 @@ const Main = (props) => {
 
   return (
     <React.Fragment>
-      <Grid bg={'#EFF6FF'} padding='20px 0px'>
+      <Grid padding='20px 0px'>
         <Grid>
           <Grid is_flex padding='4px 16px'>
             <Button
@@ -21,85 +21,56 @@ const Main = (props) => {
           <Text>님 안녕하세요!</Text>
 
           <Grid is_flex padding='4px 16px'>
-            <Grid>
+            <Grid padding='4px 16px'>
+              <Grid padding='0px 16px' bg={'#EFF6FF'}>
+                <Image
+                  width='50vh'
+                  shape='circle'
+                  src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
+                />
+                <Text bold size>
+                  luke
+                </Text>
+              </Grid>
               <Image
-                width='50vh'
-                shape='circle'
-                src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
+                shape='rectangle'
+                src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
               />
-              <Text>luke</Text>
             </Grid>
-            <img
-              width={300}
-              src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
-            />
 
-            <Grid>
+            <Grid padding='4px 16px'>
+              <Grid padding='0px 16px' bg={'#EFF6FF'}>
+                <Image
+                  width='50vh'
+                  shape='circle'
+                  src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
+                />
+                <Text bold size>
+                  luke
+                </Text>
+              </Grid>
               <Image
-                width='50vh'
-                shape='circle'
-                src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
+                shape='rectangle'
+                src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
               />
-              <Text>luke</Text>
             </Grid>
-            <img
-              width={300}
-              src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
-            />
 
-            <Grid>
+            <Grid padding='4px 16px'>
+              <Grid padding='0px 16px' bg={'#EFF6FF'}>
+                <Image
+                  width='50vh'
+                  shape='circle'
+                  src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
+                />
+                <Text bold size>
+                  luke
+                </Text>
+              </Grid>
               <Image
-                width='50vh'
-                shape='circle'
-                src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
+                shape='rectangle'
+                src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
               />
-              <Text>luke</Text>
             </Grid>
-            <img
-              width={300}
-              src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
-            />
-          </Grid>
-
-          <Grid is_flex padding='4px 16px'>
-            <Grid>
-              <Image
-                width='50vh'
-                shape='circle'
-                src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
-              />
-              <Text>luke</Text>
-            </Grid>
-            <img
-              width={300}
-              src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
-            />
-
-            <Grid>
-              <Image
-                width='50vh'
-                shape='circle'
-                src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
-              />
-              <Text>luke</Text>
-            </Grid>
-            <img
-              width={300}
-              src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
-            />
-
-            <Grid>
-              <Image
-                width='50vh'
-                shape='circle'
-                src='https://t1.daumcdn.net/cfile/tistory/997E5C3C5BA1E68137'
-              />
-              <Text>luke</Text>
-            </Grid>
-            <img
-              width={300}
-              src='https://www.bigjungbo.com/files/attach/images/163/952/878/009/70954840c442cac5c338e2e9a7ed1ce7.jpeg'
-            />
           </Grid>
         </Grid>
       </Grid>
